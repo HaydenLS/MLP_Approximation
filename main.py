92898b8fcf454ede20b0f5cc1d4a2da9bc867600
HaydenLS <lunixfang@gmail.com>
2024-12-20 20:23:57 +0300

    Added graphics to 3 methods

--- main.py
+++ main.py
@@ -67,8 +67,7 @@ class MLP:
 
         # Выходной слой
         self.z_output = self.activate_1 @ self.weights_output + self.bias_output
-        self.activate_output = self.z_output # Линейная активация
-
+        self.activate_output = self.z_output  # Линейная активация
 
         return self.activate_output
 
@@ -87,8 +86,6 @@ class MLP:
         self.weights_output -= lr * (self.activate_1.T @ delta_output)
         self.bias_output -= lr * np.sum(delta_output, axis=0, keepdims=True)
 
-
-
     # Mean Squared Error
     def MSE(self, y_true, y_pred):
         return np.mean((y_true - y_pred) ** 2)
@@ -96,21 +93,24 @@ class MLP:
     # R^2 Score
     def score(self, y_true, y_pred):
         y_true_mean = np.mean(y_true)
-        SS_tot = np.sum((y_true - y_true_mean)**2)
-        Ss_res = np.sum((y_true - y_pred)**2)
+        SS_tot = np.sum((y_true - y_true_mean) ** 2)
+        Ss_res = np.sum((y_true - y_pred) ** 2)
 
         return 1 - Ss_res / SS_tot
 
     # Стандартный градиентный спуск
     def train(self, X, y, lr, epochs, activate_function=AF.tanh, activate_derivative=AF.tanh_derivative):
+        self.loss_history = []  # Для хранения потерь
         for epoch in range(epochs):
 
             predictions = self.forward(X, activate_function)
 
             self.backward(X, y, lr, activate_derivative)
 
+            loss = self.MSE(y, predictions)
+            self.loss_history.append(loss)  # Сохранение потери
+
             if epoch % 500 == 0:
-                loss = np.mean((y - predictions) ** 2)
                 print(f"Epoch {epoch}, MSE Loss: {loss:.4f}")
 
         predictions = self.forward(X, activate_function)
@@ -118,7 +118,10 @@ class MLP:
         print(f"Model R^2 Score: {score:.4f}")
 
     # Метод SDG
-    def train_SDG(self, X, y, lr, epochs, batch_size, activate_function=AF.tanh, activate_derivative=AF.tanh_derivative):
+    def train_SDG(self, X, y, lr, epochs, batch_size, activate_function=AF.tanh,
+                  activate_derivative=AF.tanh_derivative):
+
+        self.loss_history = []  # Для хранения потерь
         for epoch in range(epochs):
             # Перемешиваем данные
             indices = np.arange(X.shape[0])
@@ -136,10 +139,10 @@ class MLP:
                 self.backward(X_batch, y_batch, lr, activate_derivative)
 
             # Логирование потерь
-
+            predictions = self.forward(X, activate_function)
+            loss = self.MSE(y_true=y, y_pred=predictions)
+            self.loss_history.append(loss)  # Сохранение потери
             if epoch % 500 == 0:
-                predictions = self.forward(X, activate_function)
-                loss = self.MSE(y_true=y, y_pred=predictions)
                 print(f"Epoch {epoch}, Loss: {loss:.4f}")
 
         # Итоговая точность нашей модели.
@@ -155,8 +158,7 @@ class DoubleLayerMLP(MLP):
         # Матрица весов 2 скрытого слоя.
         self.weights_l2 = np.random.rand(l1_dim, l2_dim) - 0.5
         # Матрица весов выходного слоя - один нейрон.
-        self.weights_output = np.random.rand(l2_dim, output_dim) # output_dim - это всегда 1
-
+        self.weights_output = np.random.rand(l2_dim, output_dim)  # output_dim - это всегда 1
 
         self.bias_l1 = np.zeros((1, l1_dim))
         self.bias_l2 = np.zeros((1, l2_dim))
@@ -175,10 +177,9 @@ class DoubleLayerMLP(MLP):
 
         # Для выходного слоя
         self.z_3 = self.activate_2 @ self.weights_output + self.bias_output
-        self.activate_output = self.z_3 # Не используем функцию активации на выходе
+        self.activate_output = self.z_3  # Не используем функцию активации на выходе
 
         return self.activate_output
-
 
     def backward(self, X, y, lr, activate_derivative):
         # Дельта на выходе
@@ -204,13 +205,22 @@ class DoubleLayerMLP(MLP):
         self.weights_output -= lr * (self.activate_2.T @ delta_output)
         self.bias_output -= lr * np.sum(delta_output, axis=0, keepdims=True)
 
+# Графики потерь
+def plot_loss(gd_loss, sgd_loss, sdg_mini_loss):
+    plt.plot(gd_loss, label="GD Loss")
+    plt.plot(sgd_loss, label="SGD Loss")
+    plt.plot(sdg_mini_loss, label="SGD Mini Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.title("GD vs SGD vs MiniSDG Loss")
+    plt.show()
 
 
-# Пример функции
+# Функция
 def func(x):
-    #return np.sin(x / 2) + np.cos(x) ** 2
-    return np.sin(x**2) + x
-
+    # return np.sin(x / 2) + np.cos(x) ** 2
+    return np.sin(x ** 2) + x
 
 
 # Генерация данных
@@ -222,20 +232,48 @@ input_dim = 1
 l1_dim = 5
 l2_dim = 5
 output_dim = 1
-model = MLP(input_dim, l1_dim, output_dim)
-#model = DoubleLayerMLP(input_dim, l1_dim, l2_dim, output_dim)
+
+# Activate Functions
+activate_function = AF.leaky_relu
+activate_derivative = AF.leaky_relu_derivative
 
 
-# Обучение модели
-activate_function = AF.tanh
-activate_derivative = AF.tanh_derivative
-#model.train(X, y_true, 0.001, 5000, activate_function, activate_derivative)
-model.train_SDG(X, y_true, 0.01, 5000, 20, activate_function, activate_derivative)
+# Градиентный спуск
+model_gd = DoubleLayerMLP(input_dim, l1_dim, l2_dim, output_dim)
+model_gd.train(X, y_true, 0.001, 5000, activate_function, activate_derivative)
+
+# Стохастический градиентный спуск
+model_sgd = DoubleLayerMLP(input_dim, l1_dim, l2_dim, output_dim)
+model_sgd.train_SDG(X, y_true, 0.01, 5000, 1, activate_function, activate_derivative)
+
+# Стохастический градиентный спуск с размером батчей
+model_sgd_mini = DoubleLayerMLP(input_dim, l1_dim, l2_dim, output_dim)
+model_sgd_mini.train_SDG(X, y_true, 0.01, 5000, 20, activate_function, activate_derivative)
+
 
 # График функции после обучения
+plt.title("Model_GD")
 plt.plot(X, y_true, label="Actual function")
-plt.plot(X, model.forward(X, activate_function), label="MLP approximation")
+plt.plot(X, model_gd.forward(X, activate_function), label="MLP approximation")
+plt.legend()
+plt.show()
+
+plt.close()
+
+plt.title("Model_SGD")
+plt.plot(X, y_true, label="Actual function")
+plt.plot(X, model_sgd.forward(X, activate_function), label="MLP approximation")
+plt.legend()
+plt.show()
+
+plt.close()
+
+plt.title("Model_SGD_Mini")
+plt.plot(X, y_true, label="Actual function")
+plt.plot(X, model_sgd_mini.forward(X, activate_function), label="MLP approximation")
 plt.legend()
 plt.show()
 
 
+
+plot_loss(model_gd.loss_history, model_sgd.loss_history, model_sgd_mini.loss_history)
